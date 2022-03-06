cbb3e8c8bd100bd96b1b096d5ccd178ab9b164f9
codewithjohnson <75730906+codewithjohnson@users.noreply.github.com>
2022-03-06 12:08:06 +0100

    adjusted

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,2 +1,180 @@
-export const questions = 'pop';
+export const questions = [
+    {
+      question: "What is the value of angular momentum quantum number of a 3d-orbital?",
+      a: 3,
+      b: 0,
+      c: 1,
+      d: 2,
+      correct: "d",
+    },
+    {
+      question: "The energy needed to remove an electron in the outermost shell of a gaseous atom is called _________",
+      a: "Ionization energy",
+      b: "Electron affinity",
+      c: "electrode potential",
+      d: "photoelectric energy",
+      correct: "a",
+    },
+    
+    {
+      question: "Substances that are electron pair acceptors are called ___________",
+      a: "Arrhenius bases	",
+      b: "Lewis acids",
+      c: "Bronsted-Lowry acids",
+      d: "Lewis bases",
+      correct: "b",
+    },
 
+    {
+      question: "The temperature above which the liquid state of a substance no longer exist is called ___________",
+      a: "Supercritical temperature",
+      b: "Critical pressure",
+      c: "Critical temperature",
+      d: "Critical point",
+      correct: "c",
+    },
+
+    {
+      question: "The part of the universe that is of immediate interest in a reaction is termed ___________",
+      a: "Universe",
+      b: "Surrounding",
+      c: "System",
+      d: "Environment",
+      correct: "c",
+    },
+
+    {
+      question: "Which of these bonds does not exist within a molecule of ammonium chloride?",
+      a: "Ionic",
+      b: "Covalent",
+      c: "Dative covalent",
+      d: "Van der Waals",
+      correct: "a",
+    },
+
+    {
+      question: "The dissolution of a non-volatile solute in a volatile solvent",
+      a: "Increase its boiling point ",
+      b: "Decreases its boiling point",
+      c: "Has no effect ",
+      d: "Increases the vapour pressure",
+      correct: "a",
+    },
+
+    {
+      question: "The element that gives no colour in flame is",
+      a: "Be",
+      b: "Ca",
+      c: "Na",
+      d: "Li",
+      correct: "a",
+    },
+
+    {
+      question: "Which of the statements below is not a property of halogen?",
+      a: "They all exhibit valency of +1",
+      b: "50.49 amu",
+      c: "They are all non-metals",
+      d: "They are all oxidizing agents",
+      correct: "a",
+    },
+
+    {
+      question: "An element which can be detected using Lassaigne's test is ___________",
+      a: "sodium",
+      b: "sulphur",
+      c: "potassium",
+      d: "magnesium",
+      correct: "b",
+    },
+
+    {
+      question: "If the molecule A - B breaks homolytically, then",
+      a: "A becomes negatively charged",
+      b: "A becomes positively charged",
+      c: "electrons are shared evenly",
+      d: "both electrons move to B because B is more electronegative",
+      correct: "c",
+    },
+
+    {
+      question: "The number of π electrons in butyne is",
+      a: 6,
+      b: 2,
+      c: 4,
+      d: 8,
+      correct: "c",
+    },
+
+    {
+      question: "In the reaction between propene and hydrogen chloride, the electrophile that initiates the reaction is ___________",
+      a: "the π electrons of the C=C bond in propene",
+      b: "the partially positive hydrogen atom from HCl",
+      c: "the partially negative chlorine atom from HCl",
+      d: "the fully positive hydrogen from HCl",
+      correct: "b",
+    },
+
+    {
+      question: "All Group I metals react with water to form",
+      a: "hydrogen gas only",
+      b: "neutral solutions",
+      c: "acidic solution with liberation of hydrogen",
+      d: "strong caustic solution with liberation of hydrogen",
+      correct: "d",
+    },
+
+    {
+      question: "Gases deviate from Charles‘law at ___________",
+      a: "high temperature",
+      b: "low pressure",
+      c: "low temperature",
+      d: "moderate pressure",
+      correct: "c",
+    },
+
+    {
+      question: "Lucas reagent is used to distinguish between",
+      a: "saturated and unsaturated hydrocarbons",
+      b: "primary and secondary amines",
+      c: "alkanols and alkanones",
+      d: "the three classes of alkanols",
+      correct: "d",
+    },
+
+    {
+      question: "Which of the following compounds will show geometrical isomerism? ___________",
+      a: "1-butane",
+      b: "2-butane",
+      c: "2-pentyne",
+      d: "1,2-dichloropropane",
+      correct: "b",
+    },
+
+    {
+      question: "2.40 g of ethanol was reacted with an excess of aqueous acidified potassium dichromate (VI). The yield of the desired organic product was 70.0 %. What mass of the desired product collected? [C = 12, O = 16, H = 1]",
+      a: "1.60 g",
+      b: "4.10 g",
+      c: "2.29 g",
+      d: "3.29 g",
+      correct: "d",
+    },
+
+    {
+      question:"Which of the following has the shortest carbon-carbon bond length? ___________",
+      a: "Propene",
+      b: "Propane",
+      c: "Propyne",
+      d: "Ethane",
+      correct: "c",
+    },
+
+    {
+      question: "The IUPAC name of the compound CH≡C–CH2–CH2–COOH is ___________",
+      a: "1-pentyne-4-oic acid",
+      b: "pentyn-1-oic acid",
+      c: "5-pentyn-1-oic acid",
+      d: "pent-4-yne-1-oic acid",
+      correct: "d",
+    },
+  ];
